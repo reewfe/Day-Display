c88be602c5a2a624bf682a78135a3cb74b0c422c
xaloph <timw80794@gmail.com>
2025-07-10 17:37:45 +0200

    fabric-1.21.6

---
+++ src/main/java/com/reewfe/daydisplay/DayDisplayClient.java
@@ -0,0 +1,10 @@
+package com.reewfe.daydisplay;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DayDisplayClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/reewfe/daydisplay/DayDisplayDataGenerator.java
@@ -0,0 +1,11 @@
+package com.reewfe.daydisplay;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DayDisplayDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
